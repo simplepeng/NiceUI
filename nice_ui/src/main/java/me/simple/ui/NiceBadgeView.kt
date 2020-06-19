1ebfc4f51edbe3772074f484a39b50d96f1b9b0c
陈鹏 <simple19930611@gmail.com>
2020-06-19 18:07:25 +0800

    update:NiceBadgeView

--- nice_ui/src/main/java/me/simple/ui/NiceBadgeView.kt
+++ nice_ui/src/main/java/me/simple/ui/NiceBadgeView.kt
@@ -2,7 +2,13 @@ package me.simple.ui
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.drawable.GradientDrawable
+import android.os.Build
 import android.util.AttributeSet
+import android.view.Gravity
 import androidx.appcompat.widget.AppCompatTextView
 
 class NiceBadgeView @JvmOverloads constructor(
@@ -11,22 +17,35 @@ class NiceBadgeView @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : AppCompatTextView(context, attrs, defStyleAttr) {
 
+    private val drawable = GradientDrawable()
+    var color: Int = Color.RED
 
     init {
-
+        gravity = Gravity.CENTER
+        isSingleLine = true
+        maxLines = 1
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 
+        if (measuredWidth <= measuredHeight) {
+            setMeasuredDimension(measuredHeight, measuredHeight)
+        } else {
+            setMeasuredDimension(measuredWidth, measuredHeight)
+        }
     }
 
-    override fun onDraw(canvas: Canvas?) {
-        drawBackground(canvas)
-        super.onDraw(canvas)
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        drawable.cornerRadius = h / 2f
+        drawable.setColor(color )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            background = drawable
+        } else {
+            setBackgroundDrawable(drawable)
+        }
     }
 
-    private fun drawBackground(canvas: Canvas?) {
-        if (canvas == null) return
-    }
 }
